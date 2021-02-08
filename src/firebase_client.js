39a2a0fcef7399e07171a48f0f0bb1245af6e329
Vitor F Dullens <vitordullens2.0@gmail.com>
2021-02-08 18:01:26 -0300

    Add firebase dependencies and a subscription

---
+++ src/firebase_client.js
@@ -0,0 +1,19 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBJ40d8bNo2x_reFNjfxCUinALH24Rzh9Y",
+  authDomain: "seer-3ec9b.firebaseapp.com",
+  databaseURL: "https://seer-3ec9b-default-rtdb.firebaseio.com",
+  projectId: "seer-3ec9b",
+  storageBucket: "seer-3ec9b.appspot.com",
+  messagingSenderId: "287984468860",
+  appId: "1:287984468860:web:0c5e8ebdbb0d8a85f25af9",
+  measurementId: "G-BJJ494Z4H4"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+const database = firebase.database()
+
+const live_report = database.ref('live_report/')
+
+export  { live_report } 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
-const config = () => {
-  var firebaseConfig = {
-    apiKey: "AIzaSyBJ40d8bNo2x_reFNjfxCUinALH24Rzh9Y",
-    authDomain: "seer-3ec9b.firebaseapp.com",
-    databaseURL: "https://seer-3ec9b-default-rtdb.firebaseio.com",
-    projectId: "seer-3ec9b",
-    storageBucket: "seer-3ec9b.appspot.com",
-    messagingSenderId: "287984468860",
-    appId: "1:287984468860:web:0c5e8ebdbb0d8a85f25af9",
-    measurementId: "G-BJJ494Z4H4"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-}
+import { live_report } from "./firebase_client.js";
+
+live_report.on('value', (snapshot) => {
+  const data = snapshot.val()
+  console.log(data)
+})
