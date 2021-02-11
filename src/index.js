e1bc7d976d652934ede3a0359a7c52f6ee6f3080
Vitor F Dullens <vitordullens2.0@gmail.com>
2021-02-11 12:38:23 -0300

    Add a separate style folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+import './styles/global.css';
 
 ReactDOM.render(
   <React.StrictMode>
