b56f0ede556e5631d49f15fc2170653f4890972c
Vitor F Dullens <vitordullens2.0@gmail.com>
2021-02-17 14:54:53 -0300

    Updates how the data is read

--- src/components/simulator/index.js
+++ src/components/simulator/index.js
@@ -1,23 +1,19 @@
-import { useEffect, useState } from "react";
-
-function Simulator( { data } ) {
-  const [dataList, setDataList] = useState([]);
-  const [numberSteps, setNumberSteps] = useState(0);
-
-  useEffect(() => {
-    setDataList(data[0]);
-  }, [data])
-
-  useEffect(() => {
-    if (dataList) {
-      setNumberSteps(dataList.length);
-    }
-  }, [dataList])
+function Simulator( { details, steps } ) {
 
   return (
-    <h2>
-      Steps: {numberSteps}
-    </h2>
+    <div>
+      <h2>
+        Details:
+      </h2>
+      <p>{JSON.stringify(details)}</p>
+      <h3>
+        Steps: 
+      </h3>
+        <p>Step count: {steps?.length}</p>
+        {steps.map((data, index) => (
+          <p key={index}>{JSON.stringify(data)}</p>
+        ))}
+    </div>
   )
 }
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,31 +4,28 @@ import firebase from '../../services/firebase';
 import './index.css';
 
 function Home() {
-
-  const [dataList, setDataList] = useState([]);
-
+  const [details, setDetails] = useState();
+  const [steps, setSteps] = useState([]);
+  
   useEffect(() => {
-      const live_report = firebase.database().ref('live_report/')
-      
-      const listener = live_report.on('value', (snapshot) => {
-        if(snapshot === undefined) return;
-        const data = snapshot.val();
-        const updateDataList = [...dataList, data];
-        setDataList(updateDataList);
-      })
+    const live_report = firebase.database().ref('live_report/')
+        
+    const listener = live_report.on('value', (snapshot) => {
+      if(snapshot === undefined) return;
+      const data = snapshot.val()? snapshot.val() : [{}];
+      setDetails(data[0]);
+      setSteps(data.slice(1))
+    })
 
-      return () => listener();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    return () => listener();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [firebase]);
-
+  
   return (
     <div className="Home">
       <header className="Home-header">
         <h1>Simulador</h1>
-        <Simulator data={dataList}/>
-        {dataList.map((data, index) => (
-          <p key={index}>{JSON.stringify(data)}</p>
-        ))}
+        <Simulator details={details} steps={steps} />
       </header>
     </div>
   );
