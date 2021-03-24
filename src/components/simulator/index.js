e0f0f002c955a45f70c20a2839f3d316531ccdb9
Vitor Dullens <vitord@lacunasoftware.com>
2021-03-24 13:39:15 -0300

    Update cell function created

--- src/components/simulator/index.js
+++ src/components/simulator/index.js
@@ -33,6 +33,15 @@ function Simulator( { details, steps, user } ) {
     graph = undefined;
   }, [user])
 
+  const updateCell = (cell, x, y, width, height, value, cleanValue, style) => {
+    cell.geometry.x = x
+    cell.geometry.y = y
+    cell.geometry.width = width
+    cell.geometry.height = height
+    cell.value = cleanValue ? cleanValue[1] : value
+    style?.startsWith('ellipse') ? cell.style = "shape=" + style : cell.style = style
+  }
+
   const processStep = (currStep) => {
     if (graph !== undefined){
       const model = graph?.getModel()
@@ -44,37 +53,31 @@ function Simulator( { details, steps, user } ) {
         if(step === 0){
           const objeto = Object.entries(currStep[ent]);
           const aux = objeto[0];
+          const cell = model.getCell(aux[0])
 
           if(aux[0] === 'timestamp') continue
 
           const {x, y, width, height, value, style} = aux[1];
           const cleanValue = value ? value.match(/>(.*)</) : ''
 
-          graph.insertVertex(parent, aux[0], cleanValue ? cleanValue[1] : value, x, y, width, height, style?.startsWith('ellipse') ? "shape=" + style   : style)
+          if(cell) updateCell(cell, x, y, width, height, value, cleanValue, style);
+          else graph.insertVertex(parent, aux[0], cleanValue ? cleanValue[1] : value, x, y, width, height, style?.startsWith('ellipse') ? "shape=" + style   : style)
+        
         } else {
 
           if(!currStep.hasOwnProperty(ent) || ent === 'timestamp') continue
-          var cell = model.getCell(ent)
-          var {x, y, width, height, value, style} = currStep[ent]
-          var cleanValue = value ? value.match(/>(.*)</) : ''
+          const cell = model.getCell(ent)
+          const {x, y, width, height, value, style} = currStep[ent]
+          const cleanValue = value ? value.match(/>(.*)</) : ''
           
           if(ent === 'deleted') {
             const remove = model.getCell(currStep[ent][0]);
             remove.style = "display:none;"
-          
           }
           
-          if (cell) {
-            // Entity already there, just update
-            cell.geometry.x = x
-            cell.geometry.y = y
-            cell.geometry.width = width
-            cell.geometry.height = height
-            cell.value = cleanValue ? cleanValue[1] : value
-            style?.startsWith('ellipse') ? cell.style = "shape=" + style : cell.style = style
-          } else {
-            graph.insertVertex(parent, ent, cleanValue ? cleanValue[1] : value, x, y, width, height, style?.startsWith('ellipse') ? "shape=" + style   : style)
-          }
+          if (cell) updateCell(cell, x, y, width, height, value, cleanValue, style);
+          else graph.insertVertex(parent, ent, cleanValue ? cleanValue[1] : value, x, y, width, height, style?.startsWith('ellipse') ? "shape=" + style   : style)
+          
         }
       }
       model.endUpdate()
